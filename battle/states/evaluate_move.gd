ad7b2a65eb2849c3cae9af527f45274fb69ad456
cinnamonmadi <cinnamon.madi@gmail.com>
2022-05-12 06:09:52 -0500

    Both death anims play at once if both familiars die at once
    Signed-off-by: cinnamonmadi <cinnamon.madi@gmail.com>

--- battle/states/evaluate_move.gd
+++ battle/states/evaluate_move.gd
@@ -15,6 +15,7 @@ const State = preload("res://battle/states/states.gd")
 const Action = preload("res://battle/states/action.gd")
 
 enum Todo {
+    ANNOUNCE,
     BURNOUT_PLAYER,
     BURNOUT_ENEMY,
     FAINT_PLAYER,
@@ -49,11 +50,19 @@ func begin(params):
 
         # Check if target familiar died
         if current_action.target_who == "player":
-            if not director.player_party.familiars[current_action.target_familiar].is_living():
-                todos.append({ "todo": Todo.FAINT_PLAYER, "familiar": current_action.target_familiar })
+            var dead_familiars = []
+            for i in range(0, min(2, director.player_party.familiars.size())):
+                if not director.player_party.familiars[i].is_living():
+                    dead_familiars.append(i)
+            if dead_familiars.size() != 0:
+                todos.append({ "todo": Todo.FAINT_PLAYER, "familiars": dead_familiars })
         if current_action.target_who == "enemy":
-            if not get_parent().enemy_party.familiars[current_action.target_familiar].is_living():
-                todos.append({ "todo": Todo.FAINT_ENEMY, "familiar": current_action.target_familiar })
+            var dead_familiars = []
+            for i in range(0, min(2, get_parent().enemy_party.familiars.size())):
+                if not get_parent().enemy_party.familiars[i].is_living():
+                    dead_familiars.append(i)
+            if dead_familiars.size() != 0:
+                todos.append({ "todo": Todo.FAINT_ENEMY, "familiars": dead_familiars })
 
     var battle_is_over = director.player_party.get_living_familiar_count() == 0 or get_parent().get_enemy_living_familiar_count() == 0
     if not battle_is_over and get_parent().actions.size() == 0:
@@ -77,27 +86,38 @@ func burnout_enemy_familiar(familiar_index: int):
         todos.push_front({ "todo": Todo.FAINT_ENEMY, "familiar": current_action.familiar })
     battle_dialog.open_and_wait("Enemy " + familiar.get_display_name() + " burned out!", get_parent().BATTLE_DIALOG_WAIT_TIME)
 
-func faint_player_familiar(familiar_index: int):
-    for action_index in range(0, get_parent().actions.size()):
-        if get_parent().actions[action_index].who == "player" and get_parent().actions[action_index].familiar == familiar_index:
-            get_parent().actions.remove(action_index)
-            break
-    player_sprites.get_child(familiar_index).visible = false
-    player_labels.get_child(familiar_index).visible = false
-    create_death_effect(player_sprites.get_child(familiar_index).position)
-    battle_dialog.open_and_wait(director.player_party.familiars[familiar_index].get_display_name() + " fainted!", get_parent().BATTLE_DIALOG_WAIT_TIME)
+func faint_player_familiar(familiars):
+    for familiar_index in familiars:
+        for action_index in range(0, get_parent().actions.size()):
+            if get_parent().actions[action_index].who == "player" and get_parent().actions[action_index].familiar == familiar_index:
+                get_parent().actions.remove(action_index)
+                break
+        player_sprites.get_child(familiar_index).visible = false
+        player_labels.get_child(familiar_index).visible = false
+        create_death_effect(player_sprites.get_child(familiar_index).position)
+        todos.insert(0, {
+            "todo": Todo.ANNOUNCE,
+            "message": director.player_party.familiars[familiar_index].get_display_name() + " fainted!" 
+        })
     player_familiar_died = true
 
-func faint_enemy_familiar(familiar_index: int):
-    for action_index in range(0, get_parent().actions.size()):
-        if get_parent().actions[action_index].who == "enemy" and get_parent().actions[action_index].familiar == familiar_index:
-            get_parent().actions.remove(action_index)
-            break
-    enemy_sprites.get_child(1 - familiar_index).visible = false
-    enemy_labels.get_child(1 - familiar_index).visible = false
-    create_death_effect(enemy_sprites.get_child(1 - familiar_index).position)
-    battle_dialog.open_and_wait("Enemy " + get_parent().enemy_party.familiars[familiar_index].get_display_name() + " fainted!", get_parent().BATTLE_DIALOG_WAIT_TIME)
+func faint_enemy_familiar(familiars):
+    for familiar_index in familiars:
+        for action_index in range(0, get_parent().actions.size()):
+            if get_parent().actions[action_index].who == "enemy" and get_parent().actions[action_index].familiar == familiar_index:
+                get_parent().actions.remove(action_index)
+                break
+        enemy_sprites.get_child(1 - familiar_index).visible = false
+        enemy_labels.get_child(1 - familiar_index).visible = false
+        create_death_effect(enemy_sprites.get_child(1 - familiar_index).position)
+        todos.insert(0, {
+            "todo": Todo.ANNOUNCE,
+            "message": "Enemy " + get_parent().enemy_party.familiars[familiar_index].get_display_name() + " fainted!"
+        })
     enemy_familiar_died = true
+
+func announce(message: String):
+    battle_dialog.open_and_wait(message, get_parent().BATTLE_DIALOG_WAIT_TIME)
 
 func create_death_effect(death_position: Vector2):
     var effect = effect_factory.create_effect(effect_factory.Effect.MONSTER_DEATH)
@@ -160,14 +180,16 @@ func end_state():
 
 func pop_next_todo():
     var next_todo = todos.pop_front()
-    if next_todo.todo == Todo.BURNOUT_PLAYER:
+    if next_todo.todo == Todo.ANNOUNCE:
+        announce(next_todo.message)
+    elif next_todo.todo == Todo.BURNOUT_PLAYER:
         burnout_player_familiar(next_todo.familiar)
     elif next_todo.todo == Todo.BURNOUT_ENEMY:
         burnout_enemy_familiar(next_todo.familiar)
     elif next_todo.todo == Todo.FAINT_PLAYER:
-        faint_player_familiar(next_todo.familiar)
+        faint_player_familiar(next_todo.familiars)
     elif next_todo.todo == Todo.FAINT_ENEMY:
-        faint_enemy_familiar(next_todo.familiar)
+        faint_enemy_familiar(next_todo.familiars)
     elif next_todo.todo == Todo.CURE_CONDITION:
         cure_condition(next_todo.who, next_todo.familiar, next_todo.condition)
 
